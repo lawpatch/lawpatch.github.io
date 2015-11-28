7ffb5c8da9c13315290dde01f89ffe59fa013330
Patrick Brown <patrickjamesbrown@gmail.com>
2015-11-28 23:16:49 +1100

    Got most of the content object done

---
+++ content_object_dictionary.js
@@ -0,0 +1,112 @@
+// This is the global object that the page gets a lot of information from.
+content = {
+
+    // UNITED STATES 
+
+    // General Provisions
+
+    "us-boilerplate": {
+        link: "https://github.com/lawpatch/us-boilerplate/blob/15815df11bd0da74d61f6df85cec6bd365c088df/us-boilerplate.md",
+        country: "United States",
+        title: "End of agreement boilerplate",
+        copyText: "The parties agree to the provisions in the [sensible boilerplate](here)",
+        parameterExplanation: "This patch doesn't assume the existence of any information in the provision doing the importation.  Just copy and paste!"
+    },
+
+    "us-interpretation": {
+        link: "https://github.com/lawpatch/us-interpretation/blob/a8e60fa226fdc1b360438787ed11321b1f0af29e/us-interpretation.md",
+        country: "United States",
+        title: "Interpretation provisions for an agreement",
+        copyText: "The parties agree to the provisions in the [interpretation provisions](here)",
+        parameterExplanation: "This patch doesn't assume the existence of any information in the provision doing the importation.  Just copy and paste!"
+    },
+    // UNITED KINGDOM 
+
+    // General Provisions
+
+    "uk-boilerplate": {
+        link: "https://github.com/lawpatch/uk-boilerplate/blob/1c32ec99f0c4d7909038de8c3f51c2a0f4d7f940/sensible-boilerplate.md",
+        country: "United Kingdom",
+        title: "End of agreement boilerplate",
+        copyText: "The parties agree to the provisions in the [sensible boilerplate](here)",
+        parameterExplanation: "This patch doesn't assume the existence of any information in the provision doing the importation.  Just copy and paste!"
+    },
+
+    "uk-interpretation": {
+        link: "https://github.com/lawpatch/uk-interpretation/blob/8b380df692eff58bfe2fea3b57414ea93617b800/uk-interpretation.md",
+        country: "United Kingdom",
+        title: "Interpretation provisions for an agreement",
+        copyText: "The parties agree to the provisions in the [interpretation provisions](here)",
+        parameterExplanation: "This patch doesn't assume the existence of any information in the provision doing the importation.  Just copy and paste!"
+    },
+
+    // AUSTRALIA
+
+    // General Provisions
+
+    "au-boilerplate": {
+        link: "https://github.com/lawpatch/au-boilerplate/blob/d5f1348ff82b3e90b96d78a5ba1225367318c42e/sensible-boilerplate.md",
+        country: "Australia",
+        title: "End of agreement boilerplate",
+        copyText: "The parties agree to the provisions in the [sensible boilerplate](here)",
+        parameterExplanation: "This patch doesn't assume the existence of any information in the provision doing the importation.  Just copy and paste!"
+    },
+
+    "au-interpretation": {
+        link: "https://github.com/lawpatch/au-interpretation/blob/84139bfbd5c3580bb215acf2435f6c2a4a608aae/au-interpretation.md",
+        country: "Australia",
+        title: "Interpretation provisions for an agreement",
+        copyText: "The parties agree to the provisions in the [interpretation provisions](here)",
+        parameterExplanation: "This patch doesn't assume the existence of any information in the provision doing the importation.  Just copy and paste!"
+    },
+
+    // Intellectual Property
+    
+    "au-ip_transfer": {
+        link: "https://github.com/lawpatch/au-ip_transfer/blob/master/au-ip_transfer.md",
+        country: "Australia",
+        title: "Transfer ownership of intellectual property",
+        copyText: "[This patch is still in draft]",
+        parameterExplanation: "[This patch is still in draft]"
+    },
+
+    "au-license_ip": {
+        link: "www.example.com",
+        country: "Australia",
+        title: "License intellectual property",
+        copyText: "[This patch is still in draft]",
+        parameterExplanation: "[This patch is still in draft]"
+    },
+
+    // Limitation Provisions
+
+    "au-limitation-0": {
+        link: "https://github.com/lawpatch/au-limitation/blob/90bb197e3299a4025af8c3db50e33b3f291e66a2/au-limitation-0.md",
+        country: "Australia",
+        title: "Limit liability as much as the law allows",
+        copyText: "[insert copy text]",
+        parameterExplanation: "This patch assumes the existence of information! Don't just copy and paste!"
+    },
+    "au-limitation-1": {
+        link: "https://github.com/lawpatch/au-limitation/blob/90bb197e3299a4025af8c3db50e33b3f291e66a2/au-limitation-1.md",
+        country: "Australia",
+        title: "Limitation of liability to a specified amount",
+        copyText: "",
+        parameterExplanation: "This patch assumes the existence of information! Don't just copy and paste!"
+    },
+    "au-limitation-2": {
+        link: "https://github.com/lawpatch/au-limitation/blob/90bb197e3299a4025af8c3db50e33b3f291e66a2/au-limitation-2.md",
+        country: "Australia",
+        title: "Limiting liability for consequential loss only",
+        copyText: "",
+        parameterExplanation: "This patch assumes the existence of information! Don't just copy and paste!"
+    },
+    "au-limitation-3": {
+        link: "https://github.com/lawpatch/au-limitation/blob/90bb197e3299a4025af8c3db50e33b3f291e66a2/au-limitation-3.md",
+        country: "Australia",
+        title: "Not limiting liability at all",
+        copyText: "",
+        parameterExplanation: "This patch assumes the existence of information! Don't just copy and paste!"
+    },
+}
+
--- modal_content.js
+++ modal_content.js
@@ -1,23 +1,3 @@
-// This is the global object that the page gets a lot of information from.
-content = {
-
-    "au-boilerplate": {
-        link: "https://github.com/lawpatch/au-boilerplate/blob/d5f1348ff82b3e90b96d78a5ba1225367318c42e/sensible-boilerplate.md",
-        country: "Australia",
-        title: "End of agreement boilerplate",
-        copyText: "The parties agree to the provisions in the [sensible boilerplate](here)",
-        parameterExplanation: "This patch doesn't assume the existence of any information in the provision doing the importation.  Just copy and paste!"
-    },
-
-    "au-interpretation": {
-        link: "https://github.com/lawpatch/au-interpretation/blob/84139bfbd5c3580bb215acf2435f6c2a4a608aae/au-interpretation.md",
-        country: "Australia",
-        title: "Interpretation provisions for an agreement",
-        copyText: "The parties agree to the provisions in the [interpretation provisions](here)",
-        parameterExplanation: "This patch doesn't assume the existence of any information in the provision doing the importation.  Just copy and paste!"
-    }
-}
-
 bootBoxFactory = function(mc) {
     var title = mc.country + ' - ' + mc.title
     var template = "{{parameterExplanation}}<br><br><div><textarea id='copy-text' rows=3 style='width:96%;padding:10px;font-size:10px;'>{{copyText}}.</textarea></div>"
